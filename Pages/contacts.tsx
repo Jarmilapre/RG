1fce2e6c3e9d6843dfb4d76d4e86f3b1806f1a4b
Jarmila Preradová <preradovaj@lmc.eu>
2019-11-23 16:05:19 +0100

    add new file contacts

---
+++ Pages/contacts.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default () => {
+  return (
+    <>
+      <Head>
+        <title>Contact me</title>
+      </Head>
+      <p>
+        I'm <a href="https://instagram.com/jsemnela">@jsemnela</a> on Instagram,
+        you can write me there. You can also follow my owners{" "}
+        <a href="https://instagram.com/ondrejsika">@ondrejsika</a> and{" "}
+        <a href="https://instagram.com/zuzjes">@zuzjes</a>.
+      </p>
+    </>
+  );
+};
