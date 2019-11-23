f80b6575957161368a5301fec893ccc39990a2d8
Jarmila Preradová <preradovaj@lmc.eu>
2019-11-23 16:02:31 +0100

    add title

--- Pages/index.tsx
+++ Pages/index.tsx
@@ -1,8 +1,23 @@
-import Hello from "../components/Hello";
+import Head from "next/head";
 export default () => {
   return (
-    <div>
-        <Hello name="Já"/>
-    </div>
+    <>
+      <Head>
+        <title>About me</title>
+      </Head>
+      <p>
+        Ahoy, I'm Nela,
+        <br />
+        I'm half labrador half swiss shepard.
+      </p>
+      <p>
+        I love watter (anytime, anywhere, dirty is better than clean), mud,
+        mountines and play fetch.
+      </p>
+      <p>
+        I was born 26th of May 2015 close to Hradec Kralove. Now, I live in
+        Prague with my best owners Ondrej and Zuzka.
+      </p>
+    </>
   );
 };
--- components/Hello.tsx
+++ components/Hello.tsx
@@ -2,10 +2,11 @@ import React, { FunctionComponent } from "react";
 
 type HelloProps = {
   name: string;
+  color?: string;
 };
 
-const Hello: FunctionComponent<HelloProps> = ({ name }) => (
-  <div style={{ color:"green"}}>
+const Hello: FunctionComponent<HelloProps> = ({ name, color }) => (
+  <div style={{ color: color || "green"}}>
     <h1>Hello {name}</h1>
   </div>
 );
